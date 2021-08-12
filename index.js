7263222d83f9c964507faa860d28fbbe4f817b1e
zexu24 <zexu@computingforall.org>
2021-08-12 14:14:37 -0700

    completed variable workshop

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello World!');
+var firstName = "Ze";
+let lastName = "Xu";
+const age = 20;
+
+
+firstName = "ayo";
+lastName = "Yeet";
+console.log('Hello ' + firstName + ' ' + lastName + '!' + ' You are ' + age + ' years old!');
